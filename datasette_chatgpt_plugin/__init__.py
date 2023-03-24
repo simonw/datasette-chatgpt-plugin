f40860638f59a52548560b38ac0527d6f67795af
Simon Willison <swillison@gmail.com>
2023-03-24 06:31:26 -0700

    name_for_model() function, refs #1

--- datasette_chatgpt_plugin/__init__.py
+++ datasette_chatgpt_plugin/__init__.py
@@ -1,1 +1,14 @@
-from datasette import hookimpl
+import hashlib
+from urllib.parse import urlparse
+
+
+def name_for_model(url):
+    name = urlparse(url).hostname.replace(".", "_").replace("-", "_")
+    md5_hash = hashlib.md5(url.encode("utf-8")).hexdigest()[:6]
+    output = f"datasette_{name}_{md5_hash}"
+
+    if len(output) > 50:
+        excess_length = len(output) - 50
+        output = f"datasette_{name[:-excess_length]}_{md5_hash}"
+
+    return output
--- tests/test_chatgpt_plugin.py
+++ tests/test_chatgpt_plugin.py
@@ -1,4 +1,5 @@
 from datasette.app import Datasette
+from datasette_chatgpt_plugin import name_for_model
 import pytest
 
 
@@ -9,3 +10,19 @@ async def test_plugin_is_installed():
     assert response.status_code == 200
     installed_plugins = {p["name"] for p in response.json()}
     assert "datasette-chatgpt-plugin" in installed_plugins
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    (
+        ("https://example.com", "datasette_example_com_c984d0"),
+        (
+            "https://congress-legislators.datasettes.com/",
+            "datasette_congress_legislators_datasettes_c_91d42f",
+        ),
+    ),
+)
+def test_name_for_model(input, expected):
+    output = name_for_model(input)
+    assert len(output) <= 50
+    assert output == expected
